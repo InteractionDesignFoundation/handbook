2537956e041daacea7e20176c40f7ec30d813457
Alies Lapatsin <web.lapateen@gmail.com>
2020-11-04 15:51:41 +0300

    #35 Use better menu structure

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,9 @@
 /**
  * This is a config to build html docs from our markdown files.
  * @see https://vuepress.vuejs.org/
+ *
+ * We use a default theme:
+ * @see https://vuepress.vuejs.org/theme/default-theme-config.html
  */
 
 module.exports = {
@@ -28,38 +31,58 @@ module.exports = {
         repo: "InteractionDesignFoundation/handbook",
         docsBranch: "master",
         editLinks: true,
-        editLinkText: "Improve this page 🔒",
+        editLinkText: "Improve this page",
         lastUpdated: "Last Updated", // string | boolean
         nav: [
             { text: "Home", link: "/" },
-            // {text: 'Guides', link: '/guides/'},
-            // {text: 'Library', link: '/library/'},
             { text: "Design System", link: "https://www.interaction-design.org/admin/design-system" }
         ],
 
         sidebarDepth: 0,
         sidebar: [
+            "/company/about",
             {
                 title: "Guides",
+                collapsable: false,
                 children: [
-                    "/guides/onboarding/onboarding"
-                    // '/servers/CI-CD-pipelines',
-                    // '/servers/CI-CD-pipelines--advanced',
-                    // '/servers/production-emergency-protocol',
+                    "/guides/expectations",
+                    {
+                        title: "Onboarding",
+                        children: [
+                            "/guides/onboarding/",
+                            "/guides/onboarding/onboarding--domain-knowledge",
+                            "/guides/onboarding/onboarding--mentorGuide",
+                            "/guides/onboarding/onboarding__forge"
+                        ]
+                    },
+                    "/guides/credentials",
+                    "/guides/collaboration-tools",
+                    {
+                        title: "Scrum",
+                        children: [
+                            "/guides/scrum/",
+                            "/guides/scrum/scrumTeam",
+                            "/guides/scrum/productOwner",
+                            "/guides/scrum/scrumMaster",
+                            "/guides/scrum/backlog"
+                        ]
+                    }
                 ]
             },
             {
                 title: "Back end",
+                collapsable: false,
                 children: [
                     "/library/back-end/clean-code-php",
-                    "/library/back-end/conventions--laravel",
                     "/library/back-end/conventions--php",
+                    "/library/back-end/conventions--laravel",
                     "/library/back-end/SOLID",
                     "/library/back-end/literature"
                 ]
             },
             {
                 title: "Front end",
+                collapsable: false,
                 children: [
                     "/library/front-end/clean-code-js",
                     "/library/front-end/conventions--js",
