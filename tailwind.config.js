8d21750429c0a4e135bcdaa5e68bd0ec481a228d
Prashant Sani <814793+prashantsani@users.noreply.github.com>
2024-07-09 14:51:37 +0530

    #0 Add Tailwind breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,22 @@ export default {
 	],
   theme: {
     extend: {},
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1056px',
+      // => @media (min-width: 1056px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
   },
   plugins: [],
 }
