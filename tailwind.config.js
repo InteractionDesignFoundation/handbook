3c28ec64d22621b1449afb861ab0f5136280a5b0
Prashant Sani <814793+prashantsani@users.noreply.github.com>
2024-07-09 17:01:34 +0530

    Tailwind config not to purge index.md

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
+    './index.md',
     './docs/.vitepress/**/*.{js,ts,vue}',
 		'./docs/**/*.md',
 		'./vitepress/**/*.{js,ts,vue}',
